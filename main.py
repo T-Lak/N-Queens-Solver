6383850c43ba4b5fb814662f058a0d3d6946c17d
acv417 <tim.spulak@haw-hamburg.de>
2023-02-07 23:31:00 +0100

    refactoring

--- main.py
+++ main.py
@@ -13,40 +13,40 @@ def display(bitboard: str) -> None:
     print('\n'.join([' '.join(wrap(line[::-1], 1)) for line in wrap(bitboard, line_width)]), '\n')
 
 
-def create_lookup_table(_size: int) -> None:
-    n_bits = 2**(_size*_size) - 1  # to truncate bits added by left shift
+def create_lookup_table(n: int) -> None:
+    n_x_n_mask = 2**(n * n) - 1  # to truncate bits added by left-shift
 
-    north_mask = create_north_mask(_size)
-    south_mask = create_south_mask(_size)
-    east_mask  = create_east_mask(_size)
-    west_mask  = create_west_mask(_size)
+    north_mask = create_north_mask(n)
+    south_mask = create_south_mask(n)
+    east_mask  = create_east_mask(n)
+    west_mask  = create_west_mask(n)
 
-    north_east_mask = create_north_east_mask(_size)
-    north_west_mask = create_north_west_mask(_size) >> (_size - 1)
-    south_east_mask = create_south_east_mask(_size) << (_size - 1)
-    south_west_mask = create_south_west_mask(_size)
+    north_east_mask = create_north_east_mask(n)
+    north_west_mask = create_north_west_mask(n) >> (n - 1)
+    south_east_mask = create_south_east_mask(n) << (n - 1)
+    south_west_mask = create_south_west_mask(n)
 
-    for square in range(_size**2):
-        _file_idx = file_idx(square, _size)
+    for square in range(n ** 2):
+        _file_idx = file_idx(square, n)
 
         north_ray = north_mask << square
-        south_ray = south_mask >> ((_size ** 2 - 1) - square)
-        east_ray  = (east_mask << square) & ~masked_west_files(_file_idx, _size)
-        west_ray  = (west_mask >> ((_size ** 2 - 1) - square)) & masked_west_files(_file_idx, _size)
+        south_ray = south_mask >> ((n ** 2 - 1) - square)
+        east_ray  = (east_mask << square) & ~masked_west_files(_file_idx, n)
+        west_ray  = (west_mask >> ((n ** 2 - 1) - square)) & masked_west_files(_file_idx, n)
 
-        north_east_ray = north_east_mask  << square & ~masked_west_files(_file_idx, _size)
-        north_west_ray = north_west_mask  << square & masked_west_files(_file_idx, _size)
-        south_east_ray = (south_east_mask >> (_size**2 - 1 - square)) & ~masked_west_files(_file_idx, _size)
-        south_west_ray = south_west_mask  >> (_size**2 - 1 - square) & masked_west_files(_file_idx, _size)
+        north_east_ray = north_east_mask  << square & ~masked_west_files(_file_idx, n)
+        north_west_ray = north_west_mask  << square & masked_west_files(_file_idx, n)
+        south_east_ray = (south_east_mask >> (n ** 2 - 1 - square)) & ~masked_west_files(_file_idx, n)
+        south_west_ray = south_west_mask >> (n ** 2 - 1 - square) & masked_west_files(_file_idx, n)
 
-        attack_bitboard = (north_ray & n_bits) | \
-                          (north_east_ray & n_bits) | \
-                          (east_ray & n_bits) | \
-                          (south_east_ray & n_bits) | \
-                          (south_ray & n_bits) | \
-                          (south_west_ray & n_bits) | \
-                          (west_ray & n_bits) | \
-                          (north_west_ray & n_bits)
+        attack_bitboard = (north_ray & n_x_n_mask) | \
+                          (north_east_ray & n_x_n_mask) | \
+                          (east_ray & n_x_n_mask) | \
+                          (south_east_ray & n_x_n_mask) | \
+                          (south_ray & n_x_n_mask) | \
+                          (south_west_ray & n_x_n_mask) | \
+                          (west_ray & n_x_n_mask) | \
+                          (north_west_ray & n_x_n_mask)
 
         ATTACK_LUT.append(attack_bitboard)
 
