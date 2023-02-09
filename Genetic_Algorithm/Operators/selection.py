b40edfbaea3e5070852d79df363ca0044211a0b6
acv417 <tim.spulak@haw-hamburg.de>
2023-02-09 16:26:04 +0100

    selection types implemented

--- Genetic_Algorithm/Operators/selection.py
+++ Genetic_Algorithm/Operators/selection.py
@@ -1,10 +1,13 @@
+import numpy as np
 from abc import ABC, abstractmethod
+
+from Genetic_Algorithm.population import Population
 
 
 class SelStrategy(ABC):
 
     @abstractmethod
-    def compute(self):
+    def compute(self, population: Population) -> list:
         pass
 
 
@@ -21,41 +24,47 @@ class SelectionContext:
     def strategy(self, strategy: SelStrategy):
         self._strategy = strategy
 
-    def execute(self):
-        self._strategy.compute()
-
-
-class DeterministicTruncation(SelStrategy):
-
-    def __init__(self) -> None:
-        pass
-
-    def compute(self):
-        pass
+    def execute(self, population: Population) -> list:
+        return self._strategy.compute(population)
 
 
 class Tournament(SelStrategy):
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, rate: int, n: int) -> None:
+        self._rate = rate
+        self._n = n
 
-    def compute(self):
-        pass
+    def compute(self, population: Population) -> list:
+        selection = []
+        while len(selection) < population.size * self._rate:
+            candidates = population.n_random_genomes(self._n)
+            candidates.sort(key=lambda g: g.fitness)
+            selection.append(candidates[-1])
+        return selection
 
 
 class RouletteWheel(SelStrategy):
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, rate) -> None:
+        self._rate = rate
 
-    def compute(self):
-        pass
+    def compute(self, population: Population) -> list:
+        selection = []
+        probabilities = [g.fitness / population.total_fitness() for g in population.genomes]
+        while len(selection) < population.size * self._rate:
+            selection.append(np.random.choice(population.genomes, p=probabilities))
+        return selection
 
 
 class RankBased(SelStrategy):
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, rate: int) -> None:
+        self._rate = rate
 
-    def compute(self):
-        pass
+    def compute(self, population: Population) -> list:
+        selection = []
+        ranks = [i + 1 for i, _ in enumerate(population.genomes)]
+        probabilities = [rank / population.size for rank in ranks]
+        while len(selection) < population.size * self._rate:
+            selection.append(np.random.choice(population.genomes, p=probabilities))
+        return selection
